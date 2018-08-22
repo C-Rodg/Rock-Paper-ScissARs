36ede46b42f682cdc9efca3841e925fbcb96bae7
Curtis <C-Rodg@users.noreply.github.com>
2018-08-21 18:45:27 -0700

    Added background images

--- docs/js/main.js
+++ docs/js/main.js
@@ -35,12 +35,14 @@
 
 	$('a[href*=\\#]').on('click', function(event) {
 		event.preventDefault();
-
-		$('html, body').animate(
-			{
-				scrollTop: $(this.hash).offset().top
-			},
-			500
-		);
+		var offset = $(this.hash).offset();
+		if (offset.top) {
+			$('html, body').animate(
+				{
+					scrollTop: offset.top
+				},
+				500
+			);
+		}		
 	});
 })();
